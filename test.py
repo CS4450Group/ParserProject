2b21370fe512058ae0e0d7b6461b1c21fcda4111
winningham <54338413+winningham@users.noreply.github.com>
2021-12-13 01:52:01 -0600

    Added a few more tests.

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
 #this is a comment
-
 if x == 3:
     this = variable
+    this1 = 1
 else if x == 4:
     e = x
 else:
     e = y
 
 x = 5
+y = 2 ^ x
+
+while x == 3 + 1:
+     this = variable
+     x = variable
+
+for x in y:
+    this = variable
+    x = this
+    
+x += 1
+x -= 1
+x *= 1
+x /= 1
+x ^= 1
+x %= 1
